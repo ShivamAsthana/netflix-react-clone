ab7f6dc2a437dbfcaf11ee912a2feb14f885eb79
abdurahim50 <45608947+abdurahim50@users.noreply.github.com>
2023-06-14 20:41:40 -0500

    Update ecs_main.tf

--- ecs_main.tf
+++ ecs_main.tf
@@ -1,14 +1,4 @@
 # Define the provider for AWS
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
 }
