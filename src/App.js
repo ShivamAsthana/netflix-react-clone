0ce59b4796b596c8e322db303afcc21537bbdf74
abdurahim50 <45608947+abdurahim50@users.noreply.github.com>
2023-06-17 11:31:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import MainComponent from "./components/Main-Component";
 import Banner from "./components/Banner";
 import UseLog from "./components/UseLog";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "./firebase/auth";
 //import { getAuth } from "./firebase/auth";
 function App(props) {
   const [LoginStatus, SetLoginStatus] = useState(false);
